f78757c9a09bc045e5273f14801c91e72e83011c
maykell1961-byte <maykell1961@gmail.com>
2026-04-01 22:15:55 -0300

    exercicio

--- 04-layout/src/App.jsx
+++ 04-layout/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Produtos from './pages/Produtos';
 
 function App() {
+  const[tela, setTela]= useState(<Home />)
   
   return (
     <div className="cont-app">
-    <header className={'cont-header'}>nome/logo/links</header>
-    <main className={'cont-main'}>aqui as coisa acontece e aparece</main>
+    <header className={'cont-header'}>
+    <nav>
+      <button className={'botoes-nav'} onClick={()=> setTela(<Home />)}>Home</button>
+      <button className={'botoes-nav'} onClick={()=> setTela(<Produtos />)}>Produto</button>
+      <button className={'botoes-nav'} onClick={()=> setTela(<Contato />)}>contato</button>
+    </nav>
+    </header>
+    <main className={'cont-main'}>
+    {tela}
+    </main>
     </div>
       
   )
---
+++ 04-layout/src/pages/Contato.jsx
@@ -0,0 +1,12 @@
+import './Paginas.css'
+
+function Contato() {
+    return (
+        <div className={'cont-contato'}>
+        <h1>contato</h1>
+            
+        </div>
+    )
+}
+
+export default Contato
