a17bbf569161a6ea239677e73498fabb7b6abcb4
maykell1961-byte <maykell1961@gmail.com>
2026-05-21 21:55:42 -0300

    projeto-cadastro

---
+++ 08-exercicio cadastro-map/projeto-cadastro/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+function App(){
+  const[]
+  const[inputFone, setInputFone] = useState('')
+  return(
+    <div className="cont-App">
+    <div className="from-cadastro">
+    <p>Cadastro de Cotatinho</p>
+      <label>foto</label>
+
+    </div>
+  
+    </div>
+  )
+
+}
+export default App
---
+++ 08-exercicio cadastro-map/projeto-cadastro/src/Componente.jsx/Contatinho.jsx
@@ -0,0 +1,11 @@
+function Contatimho({c, excluir}){
+    //console.log(c);
+    const {foto, nome, arroba, fone, id } = c //*** */
+
+    return(
+        <div className='cont-contatinho'>
+        <h2>`{nome}</h2>
+        <p>{foto}</p>
+        <img src={foto} alt={foto} style={}/></div>
+    )
+}
